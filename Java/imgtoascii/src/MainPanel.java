728035d17a81eb3063198ff378b4b48310743c9b
peanut <juntinpao27@gmail.com>
2023-08-28 15:16:26 +0800

    fixed formatting + added comments

--- Java/imgtoascii/src/MainPanel.java
+++ Java/imgtoascii/src/MainPanel.java
@@ -6,120 +6,122 @@ import java.util.Scanner;
 import javax.imageio.ImageIO;
 
 
- public class MainPanel {
-  
-   private BufferedImage image;
-   private PrintWriter pw;
-   private FileWriter fw;
+public class MainPanel {
 
-   /*private String[] asciiCode = new String[] {"$","@","B","%","8","&",
+    private BufferedImage image;
+    private PrintWriter pw;
+    private FileWriter fw;
+
+    /*private String[] asciiCode = new String[] {"$","@","B","%","8","&",
                   "W","M","#","*","o","a","h","k","b","d","p","q","w",
                   "-", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", "\"",
                   "<", ">", ",", ".", "/", "?", "0", "1", "2", "3", "4", "5",
                   "6", "7", "8", "9","l","n","c","e","f","g","i","j","r","s"
   };*/
 
-  /*private String[] asciiCode = new String[] {
+    /*private String[] asciiCode = new String[] {
     " ", ".", "-", "~", ":", "+", "*", "=", "#", "%", "&", "8", "O", "o", "0", "@",
 };*/
     private String[] asciiCode = new String[] {
-     "0","O","1","!","|"
-};
+        "0",
+        "O",
+        "1",
+        "!",
+        "|"
+    };
 
-   public MainPanel(){
+    public MainPanel() {
 
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    System.out.print("Name of text file: ");
-    String txtfile = sc.nextLine();
-   
+        // Get the desired name for the output text file
+        System.out.print("Name of text file: ");
+        String txtfile = sc.nextLine();
 
-    try {
-         
-         // Image Picker
-         FileDialog dialog = new FileDialog((Frame)null, "Select File to Open");
-         dialog.setMode(FileDialog.LOAD);
-         dialog.setVisible(true);
-         String selectedImagePath = dialog.getDirectory() + dialog.getFile();
-         dialog.dispose();
-         System.out.println(selectedImagePath + " chosen.");
 
-         pw = new PrintWriter(fw = new FileWriter((txtfile + ".txt"), true));
-         image = ImageIO.read(new File(selectedImagePath));
-         //image = ImageIO.read(MainPanel.class.getResource(selectedImagePath));
+        try {
 
-    }catch (IOException el){
+            // Image Picker
+            FileDialog dialog = new FileDialog((Frame) null, "Select File to Open");
+            dialog.setMode(FileDialog.LOAD);
+            dialog.setVisible(true);
+            String selectedImagePath = dialog.getDirectory() + dialog.getFile();
+            dialog.dispose();
+            System.out.println(selectedImagePath + " chosen.");
 
-      el.printStackTrace();
+            // Prints the file name
+            pw = new PrintWriter(fw = new FileWriter((txtfile + ".txt"), true));
+            image = ImageIO.read(new File(selectedImagePath));
 
+        } catch (IOException el) {
+
+            el.printStackTrace();
+
+        }
     }
-   }
 
+    // Method to process the image and generate ASCII art
     public void start() {
 
-      Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-      float codeRange = 255.0f / (float)asciiCode.length;
+        float codeRange = 255.0f / (float)asciiCode.length;
 
+        // Get aspect ratio dimensions from user
+        System.out.println("ASPECT RATIO");
+        System.out.println("WIDTH: ");
+        int ratioWidth = scan.nextInt();
+        System.out.println("HEIGHT: ");
+        int ratioHeight = scan.nextInt();
 
-      System.out.println("ASPECT RATIO");
-      System.out.println("WIDTH: ");
-      int ratioWidth = scan.nextInt();
-      System.out.println("HEIGHT: ");
-      int ratioHeight = scan.nextInt();
-    
-      // default values
-      //int ratioWidth = 1;
-      //int ratioHeight = 2;
+        // Loop through the image, processing blocks according to aspect ratio
+        for (int row = 0; row < image.getHeight(); row += ratioHeight) {
+            for (int col = 0; col < image.getWidth(); col += ratioWidth) {
 
-      for (int row = 0; row < image.getHeight(); row += ratioHeight) {
-        for (int col = 0; col < image.getWidth(); col += ratioWidth) {
+                // Calculate average grayscale value for the block
+                double totgrayscaleval = 0;
+                int pixInBlock = 0;
+                for (int r = row; r < row + ratioHeight && r < image.getHeight(); r++) {
+                    for (int c = col; c < col + ratioWidth && c < image.getWidth(); c++) {
+                        Color color = new Color(image.getRGB(c, r));
+                        double grayScaleVal = color.getRed() * 0.21 + color.getGreen() * 0.72 + color.getBlue() * 0.07;
+                        totgrayscaleval += grayScaleVal;
+                        pixInBlock++;
+                    }
+                }
+                double avggscale = totgrayscaleval / pixInBlock;
 
-          // RGB
-          double totgrayscaleval = 0;
-          int pixInBlock = 0;
+                // Determine the appropriate ASCII character based on grayscale value
+                int index = (int)(avggscale / codeRange);
 
-          for (int r = row; r < row + ratioHeight && r < image.getHeight(); r++) {
-            for (int c = col; c < col + ratioWidth && c < image.getWidth(); c++) {
-                Color color = new Color(image.getRGB(c, r));
-                double grayScaleVal = color.getRed() * 0.21 + color.getGreen() * 0.72 + color.getBlue() * 0.07;
-                totgrayscaleval += grayScaleVal;
-                pixInBlock++;
+                // Print the ASCII character multiple times to match aspect ratio width
+                for (int i = 0; i < ratioWidth; i++) {
+                    try {
+                        pw.print(asciiCode[index]);
+                        pw.flush();
+                        fw.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.print(asciiCode[index] + " ");
+                }
             }
+            // Move to the next line after processing a row of ASCII art
+            System.out.print("\n");
+            try {
+                pw.println("");
+                pw.flush();
+                fw.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
-         double avggscale = totgrayscaleval / pixInBlock;
-
-      
-          int index = (int) (avggscale / codeRange);
-
-          for (int i = 0; i < ratioWidth; i++) {
-          try {
-              pw.print(asciiCode[index]);
-              pw.flush();
-              fw.flush();
-          }catch (IOException e){
-            e.printStackTrace();
-          }
-
-          System.out.print(asciiCode[index]+" ");
-        }
-      }
-
-        System.out.print("\n");
-        try{
-          pw.println("");
-          pw.flush();
-          fw.flush();
-        }catch (IOException e){
-          e.printStackTrace();
-        }
-       
-      }
     }
 
     public static void main(String[] args) {
-      MainPanel mainPanel = new MainPanel();
-      mainPanel.start();
-  }
+        MainPanel mainPanel = new MainPanel();
+        mainPanel.start();
+    }
 }
-
