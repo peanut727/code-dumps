7e99e37f38752c3da90bff3d37848076d9e069f5
peanut <juntinpao27@gmail.com>
2023-09-19 21:54:13 +0800

    odd or even using regex

--- Java/regexOddOrEven.java
+++ Java/regexOddOrEven.java
@@ -12,24 +12,31 @@ public class regexOddOrEven {
 
         String evenReg = "[02468]";
         String oddReg  = "[13579]";
+        
 
         Pattern evenPatt = Pattern.compile(evenReg);
         Pattern oddPatt = Pattern.compile(oddReg);
+        
 
         Matcher evenMatch = evenPatt.matcher(input);
         Matcher oddMatch = oddPatt.matcher(input);
+        
 
         boolean containsEven = evenMatch.find();
         boolean containsOdd = oddMatch.find();
+        
 
+    
         if (containsEven && !containsOdd) {
             System.out.println("Even");
         } else if (!containsEven && containsOdd) {
             System.out.println("Odd");
+        
         } else {
-            System.out.println("Both even and odd");
+            System.out.print("contains both even and odd");
+
+        }
         }
     }
-}
 
 
